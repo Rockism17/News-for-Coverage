dffd327c41d3705be05ff4e192148c67b178df28
Rockism17 <rocky.rui@atb.com>
2026-04-15 16:10:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,79 @@ from datetime import datetime
 
 # --- 1. CONFIGURATION ---
 WATCHLIST_GROUPS = {
-    "Alaris": ["Hammond Power", "Dry Type Transformer", "Hyundai Electric", "AEG Power Systems", "Uninterruptible Power Supply", "Schneider Electric", "Siemens", "Eaton Corp"],
-    "Tantalus": ["Tantalus", "Tantalus Systems", "Smart metering systems", "Itron", "Landis + Gyr", "Grid Tech",],
-    "Kraken": ["Kraken", "PNG", "REMUS", "NAVSEA", "Lockheed Martin", "L3Harris", "Airbus", "Rtx defense", "Northrop Grumman", "Thales Defense", "Subsea battery", "Kongsberg", "Cellula Robotics", "Huntington Ingalls", "Teledyne Technologies", "Anduril""Covelya", "Sonardyne", "EIVA", "Forcys", "Wavefront", "Chelsea Technologies", "Voyis"],
-    "5N Plus": ["5N Plus", "VNP", "Germanium", "Tellurium", "Cadmium", "AZUR Space", "Sierra Space", "Bismuth", "USA Rare Earth", "MP Materials", "First Solar"],
-    "ISC": ["ISC", "Information Services Corp", "Dye & Durham", "Saskatchewan Registry", "Canada Business Registry"],
-    "Neo Performance": ["NEO", "Neo Performance Materials", "Rare Earth Oxides", "Rare Earth Minerals"],
-    "Polaris": ["Polaris Renewable Energy", "PIF"],
-    "DIRTT": ["DIRTT", "DRT"],
-    "Biorem": ["BRM", "Biorem"],
-    "Atlas": ["AEP", "Atlas Engineered Products", "Canadian Roof Trusses", "Canadian Building Products"],
-    "Calian": ["CGY", "Calian", "Arctic Sovereignty", "Canadian Armed Forces", "Canadian Defense", "Arctic War", "Canadian Navy"],
-   "All": ["CGY", "Calian","Hammond Power", "Dry Type Transformer", "Hyundai Electric", "AEG Power Systems", "Uninterruptible Power Supply", "Schneider Electric", "Siemens", "Eaton Corp", "Tantalus", "Tantalus Systems", "Smart metering systems", "Itron", "Landis + Gyr", "Kraken", "PNG", "REMUS", "NAVSEA", "Lockheed Martin", "L3Harris", "Airbus", "Rtx defense", "Northrop Grumman", "Thales Defense", "Subsea battery", "Kongsberg", "Cellula Robotics", "Huntington Ingalls", "Teledyne Technologies", "Anduril", "Covelya", "Sonardyne", "EIVA", "Forcys", "Wavefront", "Chelsea Technologies", "Voyis", "Grid Tech", "ISC", "Information Services Corp", "Dye & Durham", "Saskatchewan Registry", "Canada Business Registry", "Polaris Renewable Energy", "PIF", "AEP", "Atlas Engineered Products", "Canadian Roof Trusses", "Canadian Building Products",  ],
+    "Alaris": ["Alaris", "Alaris Equity Partners", "3E, LLC", "Accscient, LLC", "Amur Financial Group Inc.", "Berg Demo Holdings, LLC", "Body Contour Centers, LLC (SonoBello)", "Carey Electric Contracting, LLC", 
+               "Cresa, LLC", "DNT Construction, LLC", "Edgewater Technical Associates, LLC", "Fleet Advantage, LLC", "Federal Management Partners, LLC (FMP)", "GWM Holdings, Inc. (GlobalWide Media)", 
+               "Heritage Restoration, LLC", "Kubik, LP", "LMS Reinforcing Steel Group", "McCoy Roofing Holdings LLC", "Ohana Growth Partners, LLC", "Optimus SBR", 
+               "Professional Electric Contractors of Connecticut, Inc. (PEC)", "Sagamore Plumbing and Heating LLC", "SCR Mining & Tunnelling L.P.", "The Shipyard, LLC", "Unify Consulting, LLC", 
+               "Vehicle Leasing Holdings, LLC (D&M Leasing)", "3E", "Accscient", "Amur", "Berg Demo", "SonoBello", "Carey Electric", "Cresa", "DNT", "Edgewater", "Fleet Advantage", "FMP", "GlobalWide", 
+               "Heritage", "Kubik", "LMS", "McCoy Roofing", "Ohana", "Optimus SBR", "PEC", "Sagamore", "SCR", "The Shipyard", "Unify", "D&M Leasing"],
+    
+    "Bridgemarq (BRE)": ["Bridgemarq Real Estate Services", "BRE.TO", "Royal LePage", "Proprio Direct", "Via Capitale", "Spencer Enright", "Phil Soper", "Johnston & Daniel"],
+    
+    "Canaccord (CF)": ["Canaccord Genuity", "CF.TO", "Dan Daviau", "Canaccord Wealth Management", "Sawaya Partners", "Questrade partner", "Canaccord Genuity Group"],
+    
+    "Diversified Royalties (DIV)": ["Diversified Royalty Corp", "DIV.TO", "Sean Morrison", "Mr. Lube", "Air Miles", "Sutton Group", "Nurse Next Door", "Oxford Learning", "BarBurrito", "Cheba Hut", "Mr. Mikes"],
+    
+    "Dominion Lending (DLCG)": ["Dominion Lending Centres", "DLCG.TO", "Gary Mauris", "Mortgage Architects", "MCC Mortgage Centre", "Newton Connectivity", "Chris Kayat"],
+    
+    "Exchange Income (EIF)": [
+        # Corporate & Leadership
+        "Exchange Income Corp", "EIF.TO", "Mike Pyle", "Adam Terwin", "Jake Trainor",
+        # Aviation Segment (Air Operators & Aerospace)
+        "Canadian North", "PAL Aerospace", "PAL Airlines", "Perimeter Aviation", 
+        "Calm Air", "Bearskin Airlines", "Keewatin Air", "Regional One", 
+        "Custom Helicopters", "Moncton Flight College", "Newfoundland Helicopters", 
+        "Air Borealis", "Mach2", "BC Medevac",
+        # Manufacturing Segment
+        "Northern Mat and Bridge", "Spartan Mat", "Spartan Composites", 
+        "WesTower Communications", "Quest Window Systems", "BVGlazing Systems", 
+        "Ben Machine Products", "Stainless Fabrication", "DryAir Manufacturing", 
+        "Hansen Industries", "Overlanders Manufacturing", "LV Control Mfg", 
+        "Water Blast Manufacturing", "Duhamel Sawmill"
+    ],
+    
+    "Fairfax (FFH)": ["Fairfax Financial Holdings", "FFH.TO", "Prem Watsa", "Odyssey Group", "Allied World", "Northbridge Financial", "Crum & Forster", "Brit Insurance"],
+    
+    "goeasy (GSY)": ["goeasy Ltd", "GSY.TO", "Jason Mullins", "easyfinancial", "easyhome", "LendCare"],
+    
+    "Propel (PRL)": ["Propel Holdings", "PRL.TO", "Clive Kinross", "CreditFresh", "MoneyKey", "Fora Credit", "QuidMarket", "FreshLine"],
+    
+    "RFA Financial (RFA)": ["RFA Financial Inc", "RFA.TO", "RFA Bank of Canada", "RFA Mortgage", "RFA REIT", "Holloway Lodging"],
+
+    "Trisura (TSU)": ["Trisura Group", "TSU.TO", "David Clare", "Trisura Guarantee Insurance", "Trisura Specialty", "Chris Sekine"],
+
+    "Versabank (VSB)": ["VersaBank", "VSB.TO", "David Taylor", "DRT Cyber", "Structured Receivable"],
+    
+    "Westaim (WED)": ["Westaim Corporation", "WED.TO", "Cameron MacDonald", "Skyward Specialty", "Arena Investors", "Arena Wealth Management"],
+    
+   "All": ["Alaris", "Alaris Equity Partners", "3E, LLC", "Accscient, LLC", "Amur Financial Group Inc.", "Berg Demo Holdings, LLC", "Body Contour Centers, LLC (SonoBello)", "Carey Electric Contracting, LLC", 
+               "Cresa, LLC", "DNT Construction, LLC", "Edgewater Technical Associates, LLC", "Fleet Advantage, LLC", "Federal Management Partners, LLC (FMP)", "GWM Holdings, Inc. (GlobalWide Media)", 
+               "Heritage Restoration, LLC", "Kubik, LP", "LMS Reinforcing Steel Group", "McCoy Roofing Holdings LLC", "Ohana Growth Partners, LLC", "Optimus SBR", 
+               "Professional Electric Contractors of Connecticut, Inc. (PEC)", "Sagamore Plumbing and Heating LLC", "SCR Mining & Tunnelling L.P.", "The Shipyard, LLC", "Unify Consulting, LLC", 
+               "Vehicle Leasing Holdings, LLC (D&M Leasing)", "3E", "Accscient", "Amur", "Berg Demo", "SonoBello", "Carey Electric", "Cresa", "DNT", "Edgewater", "Fleet Advantage", "FMP", "GlobalWide", 
+               "Heritage", "Kubik", "LMS", "McCoy Roofing", "Ohana", "Optimus SBR", "PEC", "Sagamore", "SCR", "The Shipyard", "Unify", "D&M Leasing",
+           "Bridgemarq Real Estate Services", "BRE.TO", "Royal LePage", "Proprio Direct", "Via Capitale", "Spencer Enright", "Phil Soper", "Johnston & Daniel",
+           "Canaccord Genuity", "CF.TO", "Dan Daviau", "Canaccord Wealth Management", "Sawaya Partners", "Questrade partner", "Canaccord Genuity Group",
+           "Diversified Royalty Corp", "DIV.TO", "Sean Morrison", "Mr. Lube", "Air Miles", "Sutton Group", "Nurse Next Door", "Oxford Learning", "BarBurrito", "Cheba Hut", "Mr. Mikes",
+           "Dominion Lending Centres", "DLCG.TO", "Gary Mauris", "Mortgage Architects", "MCC Mortgage Centre", "Newton Connectivity", "Chris Kayat",
+           "Exchange Income Corp", "EIF.TO", "Mike Pyle", "Adam Terwin", "Jake Trainor",
+               "Canadian North", "PAL Aerospace", "PAL Airlines", "Perimeter Aviation", 
+                "Calm Air", "Bearskin Airlines", "Keewatin Air", "Regional One", 
+                "Custom Helicopters", "Moncton Flight College", "Newfoundland Helicopters", 
+                "Air Borealis", "Mach2", "BC Medevac",
+                "Northern Mat and Bridge", "Spartan Mat", "Spartan Composites", 
+                "WesTower Communications", "Quest Window Systems", "BVGlazing Systems", 
+                "Ben Machine Products", "Stainless Fabrication", "DryAir Manufacturing", 
+                "Hansen Industries", "Overlanders Manufacturing", "LV Control Mfg", 
+                "Water Blast Manufacturing", "Duhamel Sawmill",
+           "Fairfax Financial Holdings", "FFH.TO", "Prem Watsa", "Odyssey Group", "Allied World", "Northbridge Financial", "Crum & Forster", "Brit Insurance",
+           "goeasy Ltd", "GSY.TO", "Jason Mullins", "easyfinancial", "easyhome", "LendCare",
+           "Propel Holdings", "PRL.TO", "Clive Kinross", "CreditFresh", "MoneyKey", "Fora Credit", "QuidMarket", "FreshLine",
+           "RFA Financial Inc", "RFA.TO", "RFA Bank of Canada", "RFA Mortgage", "RFA REIT", "Holloway Lodging",
+           "Trisura Group", "TSU.TO", "David Clare", "Trisura Guarantee Insurance", "Trisura Specialty", "Chris Sekine",
+           "VersaBank", "VSB.TO", "David Taylor", "DRT Cyber", "Structured Receivable",
+           "Westaim Corporation", "WED.TO", "Cameron MacDonald", "Skyward Specialty", "Arena Investors", "Arena Wealth Management",
+           ],
 }
 
 # The Default Blacklist (Hidden automatically)
@@ -29,7 +90,7 @@ PREMIUM_SOURCES = ["The Globe and Mail", "Bloomberg", "Reuters", "Financial Post
 
 LOGO_URL = "https://cormark.com/Portals/_default/Skins/Cormark/Images/Cormark_4C_183x42px.png"
 
-st.set_page_config(page_title="Purdchuk News Screener", page_icon=LOGO_URL, layout="wide")
+st.set_page_config(page_title="DivFin News Screener", page_icon=LOGO_URL, layout="wide")
 st.logo(LOGO_URL, link="https://cormark.com/")
 
 if 'news_data' not in st.session_state:
@@ -37,7 +98,7 @@ if 'news_data' not in st.session_state:
 
 # --- 2. SIDEBAR: THE DUAL-FILTER SYSTEM ---
 with st.sidebar:
-    st.title("Purdchuk Settings")
+    st.title("DivFin Settings")
     selected_group = st.selectbox("Watchlist Category", options=list(WATCHLIST_GROUPS.keys()))
     
     st.divider()
@@ -87,7 +148,7 @@ def get_google_news(company_name):
     return results
 
 # --- 4. MAIN UI & LOGIC ---
-st.title("Purdchuk News Screener")
+st.title("DivFin News Screener")
 st.subheader(f"Current Watchlist: {selected_group}")
 
 if st.button(f" Search {selected_group} List", use_container_width=True):
